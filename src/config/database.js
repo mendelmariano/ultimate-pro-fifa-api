14bdabbb16263a873a01abce81eb2a536a48475b
C1326383 Mendel Mariano de Castro <C1326383@interno.bb.com.br>
2023-10-11 15:19:13 -0300

    Configurações de sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgresql',
-    database: process.env.DB_DATABASE || 'ultimateprofifa',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgresql',
+    database: 'ultimateprofifa',
     define: {
         timestamps: true,
         underscored: true,
