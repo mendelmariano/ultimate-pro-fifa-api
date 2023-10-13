899001504d999373b88e1bebf7daa7a6615efc3a
C1326383 Mendel Mariano de Castro <C1326383@interno.bb.com.br>
2023-10-13 16:18:51 -0300

    Versão final com código totalmente adequado

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,22 @@
 module.exports = {
     dialect: 'postgres',
+    port: process.env.DB_PORT || '5432',
     host: process.env.DB_HOST || 'localhost',
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgresql',
     database: process.env.DB_DATABASE || 'ultimateprofifa',
+    dialectOptions: {
+        // Defina ssl apenas se DB_SSL for true
+        ...(process.env.DB_SSL === 'true'
+            ? {
+                  ssl: {
+                      ssl: true,
+                      require: true,
+                      rejectUnauthorized: false,
+                  },
+              }
+            : {}),
+    },
     define: {
         timestamps: true,
         underscored: true,
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,5 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
-import databaseConfigProd from '../config/database_prod';
 import User from '../app/models/User';
 import Team from '../app/models/Team';
 import Profile from '../app/models/Profile';
@@ -25,21 +24,7 @@ class Database {
     }
 
     init() {
-        /* this.connection = new Sequelize(
-            `${process.env.DATABASE_URL}?sslmode=require`,
-            {
-                dialect: 'postgres',
-                dialectOptions: {
-                    ssl: {
-                        rejectUnauthorized: false, // very important
-                    },
-                },
-            }
-        ); */
-
-        this.connection = new Sequelize(databaseConfigProd);
-
-        // this.connection = new Sequelize(databaseConfig);
+        this.connection = new Sequelize(databaseConfig);
 
         models
             .map((model) => model.init(this.connection))
